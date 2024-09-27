1ca3375a9edb18d1020ffd70557fd379d26580d6
Livi4-GIT <lilirioseveri20@gmail.com>
2024-09-27 01:17:05 +0000

    DEU MERDA TOTAL

--- Projeto_1.c
+++ Projeto_1.c
@@ -70,17 +70,7 @@ int Novo_usuario(){
     return 0;
 
 }
-int Saldo(){
-    printf("^^^^^^^^^^^^^^^^^^Carteira de investimentos^^^^^^^^^^^^^^^^^^^^^^\n");
-    printf("Saldo R$: \n");
-    printf("Saldo Bitcoins: \n");
-    printf("Saldo Etherum: \n");
-    printf("Saldo Ripple: \n");
-
-
-}
-
-int Deposito(){
+float Deposito(){
     float valor_dep;
     printf("^^^^^^^^^^^^^^^^^^Deposito^^^^^^^^^^^^^^^^^^^^^^\n");
     printf("Qual seria o valor de deposito: ");
@@ -88,28 +78,35 @@ int Deposito(){
     return valor_dep;
 
 }
-int Saque(){
-    float valor_saq_pre, valor_saq;
+
+
+float Saque(float valor_Total){
+    float valor_saq_pre, valor_saq, dinh;
     int senha_conf;
+    dinh = Saldo(valor_Total);
     printf("^^^^^^^^^^^^^^^^^^Saque^^^^^^^^^^^^^^^^^^^^^^\n");
     printf("Qual seria o valor de deposito: ");
     scanf("%f", &valor_saq_pre);
     printf("Confirme a senha: ");
     scanf("%d", &senha_conf);
     //colocar a comparação da senha inserida com a do arquivo
-//     while (1){
-//     if (senha_conf != senha_do_arquivo){
-//         printf("Senha incorreta!\n");
-//         printf("Digite novamente: ");
-//         scanf("%d", &senha_conf);
-//     }else{
-//         valor_saq = valor_saq_pre;
-//         return valor_saq;
-//         break;
-//     }
-// }
-    
-    
+//      while (1){
+//      if (senha_conf != senha_do_arquivo){
+//          printf("Senha incorreta!\n");
+//          printf("Digite novamente: ");
+//          scanf("%d", &senha_conf);
+//      }else{if(dinh >= valor_saq_pre){
+//          valor_saq = valor_saq_pre;
+//          return valor_saq;
+//          break;
+//          }else{
+//             printf("Saldo Insuficiente!");
+//             prinft("Tente Novamente.");
+//             break;
+//         }
+
+//      }
+//  }
 }
 int Console(){
     printf("````````````````````````````````````````````````````````````````````````````````````````````\n");
@@ -129,9 +126,69 @@ int Console(){
 int Extrato(){
 
 }
-int ComprarC(){
+float ComprarC(valor_Total){
+    float valorCompra, taxa, totalPago, saldo_reais, saldo_bitcoin, saldo_ethereum, saldo_ripple, dinh; // valores da compra e das taxas
+    int moedaEscolhida; // moeda selecionada
+    dinh = Saldo(valor_Total);
 
+    printf("^^^^^^^^^^^^^^^^^^Compra de Cripitomoedas^^^^^^^^^^^^^^^^^^^^^^\n");
+    // pede qual moeda
+    printf("Selecione a moeda para compra: \n");
+    printf("1. Bitcoin\n2. Ethereum\n3. Ripple\n");
+    printf("Digite o número: ");
+    scanf("%d", &moedaEscolhida);
+    printf("Valor a gastar: ");
+    scanf("%f", &valorCompra);
+
+    // processa a compra com base na moeda escolhida
+    switch (moedaEscolhida) {
+        case 1: // bit
+            taxa = valorCompra * 0.02; // taxa de 2%
+            totalPago = valorCompra + taxa; // total a pagar
+            if (totalPago <= dinh) { // cerifica saldo
+                saldo_reais += totalPago; // atualiza saldo
+                saldo_bitcoin += valorCompra; // adiciona bitcoin
+                printf("Comprado %.2f Bitcoin! Taxa: %.2f\n", valorCompra, taxa);
+                return saldo_reais, saldo_bitcoin;
+            } else {
+                printf("Saldo insuficiente.\n");
+            }
+            break;
+
+        case 2: // ethereum
+            taxa = valorCompra * 0.01; // Taxa de 1%
+            totalPago = valorCompra + taxa;
+            if (totalPago <= dinh) {
+                saldo_reais += totalPago;
+                saldo_ethereum += valorCompra; // adiciona etherem
+                printf("Comprado %.2f Ethereum! Taxa: %.2f\n", valorCompra, taxa);
+                return saldo_reais, saldo_ethereum;
+            } else {
+                printf("Saldo insuficiente.\n");
+            }
+            break;
+
+        case 3: // ripple
+            taxa = valorCompra * 0.01; // taxa de 1%
+            totalPago = valorCompra + taxa;
+            if (totalPago <= dinh) {
+                saldo_reais += totalPago;
+                saldo_ripple += valorCompra; // adiciona ripple
+                printf("Comprado %.2f Ripple! Taxa: %.2f\n", valorCompra, taxa);
+                return saldo_reais, saldo_ripple;
+            } else {
+                printf("Saldo insuficiente.\n");
+            }
+            break;
+
+        default: // opcao invalida
+            printf("Opção não reconhecida.\n");
+            break;
+    }
+
+    return  saldo_reais, saldo_ripple, saldo_ethereum, saldo_bitcoin;
 }
+
 
 int VenderC(){
 
@@ -159,6 +216,25 @@ int Atualizar(){
     return cotacao1; coatcao2; cotacao3;  
 
 }
+float Saldo(float valor_dep, float valor_saq, float saldo_reais, float saldo_ripple, float saldo_ethereum, float saldo_bitcoin){
+    float valor_teste, valor_Total, dinheiro_depositado, dinheiro_sacado,dinheiro_cripto;
+    dinheiro_depositado = Deposito(valor_dep);
+    dinheiro_sacado = Saque(valor_saq);
+    dinheiro_cripto = ComprarC(saldo_reais);
+
+
+    valor_Total = dinheiro_depositado - (dinheiro_sacado) - (dinheiro_cripto);
+    
+    printf("^^^^^^^^^^^^^^^^^^Carteira de investimentos^^^^^^^^^^^^^^^^^^^^^^\n");
+
+    printf("Saldo R$: \n");
+    printf("Saldo Bitcoins:\n");
+    printf("Saldo Etherum: \n");
+    printf("Saldo Ripple: \n");
+
+    return valor_Total;
+}
+
 
 int main(){
     srand(time(NULL));//pega o horario do computador para gerar cada vez que rodar um numero diferente do outro
